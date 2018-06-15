089e5a36b27f7e63931dcb3d6f21c1fa39f44e17
MERACHAIN.ORG <30058622+Merachain@users.noreply.github.com>
2018-06-15 23:50:33 +0500

    Update clientversion.h

--- clientversion.h
+++ clientversion.h
@@ -17,3 +17,23 @@
 #define DO_STRINGIZE(X) #X
 
 #endif // CLIENTVERSION_H
+rom os import path
+
+
+WHITELIST_NUM = 5000
+# In case we have bad actors.
+WHITELSIT_SUBSTITUTE_NUM = 500
+
+
+def get_user_keys_and_scores() -> [(str, float)]:
+    """KYC-cleared users with score >= 60."""
+    dir_path = path.dirname(path.realpath(__file__))
+
+    with open(path.join(dir_path, 'applicants.csv')) as f:
+        lines = f.readlines()
+    ret = []
+    # Skip header row.
+    for line in lines[1:]:
+        k, s = line.strip().split(',')
+        ret.append((k, float(s)))
+    return ret
